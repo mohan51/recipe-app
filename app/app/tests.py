0b6abcee3b8d4e28716340624676cb073dfc520c
mohan51 <mohanakrishna551@gmail.com>
2022-12-14 18:12:24 +0530

    Deleted spaces from test

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,6 @@ from app import calc
 class CalcTest(SimpleTestCase):
 
     def test_add_two_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
